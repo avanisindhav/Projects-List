3c638413a9cd51c09cf6031628c68e9fc768195e
Avani Sindhav <avani@simformsolutions.com>
2022-08-05 13:13:55 +0530

    webpack added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 // Code goes here!
-import { ProjectInput } from "./components/project-input.js";
-import { ProjectList } from "./components/project-list.js";
+import { ProjectInput } from "./components/project-input";
+import { ProjectList } from "./components/project-list";
 
 new ProjectInput();
 new ProjectList("active");
